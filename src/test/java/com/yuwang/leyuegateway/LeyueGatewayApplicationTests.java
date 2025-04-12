a57368dfa7aff8151411986b16732bca9665ab54
17854278830 <508284405@qq.com>
2025-04-12 15:34:36 +0800

    feat: 添加 Leyue Gateway 项目初始结构
    - 创建项目基础目录结构
    - 添加 Spring Boot 和 Spring Cloud 相关配置
    -配置 Nacos 发现和配置
    - 设置日志级别
    - 添加单元测试
    - 创建 Maven构建配置
    - 设置 Git属性和忽略规则

---
+++ src/test/java/com/yuwang/leyuegateway/LeyueGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuwang.leyuegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeyueGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
